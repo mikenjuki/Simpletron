5facf038e10a6fb36c9dbafef54487b7f396e807
Mike Njuki <mikenjuki@gmail.com>
2023-11-20 08:54:41 +0300

    Add project files.

---
+++ Main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//input-ouput
+#define READ 10
+#define WRITE 11
+
+//load/store operations
+#define LOAD 20
+#define STORE 21
+
+//arithmetic operations
+#define ADD 30
+#define SUBTRACT 31
+#define DIVIDE 32
+#define MULTIPLY 33
+
+//transfer of control operations
+#define BRANCH 40
+#define BRANCHHNEG 41
+#define BRANCHZERO 42
+#define HALT 43
+
+int main()
+{
+	printf("Simpletron Program");
+
+	return 0;
+}
