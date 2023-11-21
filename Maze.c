e5ef80049ca1f0a3cacd1536994c022e2e176259
Mike Njuki <mikenjuki@gmail.com>
2023-11-21 11:54:04 +0300

    create maze with random paths

--- Maze.c
+++ Maze.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 #define MSIZE 12
 
@@ -7,6 +9,9 @@ char theMaze[MSIZE][MSIZE];
 
 int mazeTraverse(int[][12]);
 void printMaze();
+void initializeMaze();
+void randomCoordinate();
+
 
 char Maze[MSIZE][MSIZE];
 
@@ -16,33 +21,63 @@ int maze()
 	printf("\n");
 	printf("Maze Program\n");
 
-	//initialize maze
-	for (int i = 0; i < MSIZE; i++) {
-		for (int j = 0; j < MSIZE; j++) {
-			theMaze[i][j] = '#';
-		}
-	}
+	srand((unsigned int)time(NULL));
 
+	initializeMaze();
 	printMaze();
+
+	randomCoordinate();
+
 
 	return 0;
 }
 
+void initializeMaze() {
+
+
+	for (int i = 0; i < MSIZE; i++) {
+		for (int j = 0; j < MSIZE; j++) {
+			int randomRow = 1 + rand() % (MSIZE - 2);
+			int randomCol = 1 + rand() % (MSIZE - 2);
+
+			theMaze[i][j] = '#';
+
+			if (i == MSIZE - 1 || i == 0 || j == MSIZE - 1 || j == 0) 
+				theMaze[i][j] = '#';
+			
+			if (randomCol && randomRow)	
+					theMaze[randomRow][randomCol] = '.';
+				
+		}
+	}
+
+}
+
+
 void printMaze() {
 	for (int i = 0; i < MSIZE; i++) {
 		for (int j = 0; j < MSIZE; j++) {
-			if (i == MSIZE - 1) {
-				printf("*  ");
-			}
-			else {
+	
 				printf("%c  ", theMaze[i][j]);
-			}
+			
 		}
 		printf("\n\n");
 	}
 }
 
-int mazeTraverse(int array[MSIZE][MSIZE])
+int  mazeTraverse(int array[MSIZE][MSIZE])
 {
 	return 0;
 }
+
+
+void randomCoordinate() {
+	int randomRow = 1 + rand() % (MSIZE - 2); 
+	int randomCol = 1 + rand() % (MSIZE - 2); 
+
+	printf("\nRandom Coordinate: theMaze[%d][%d]\n", randomRow, randomCol);
+	printf("Value at the random coordinate: %c\n", theMaze[randomRow][randomCol]);
+}
+
+
+//if no way out, say that
